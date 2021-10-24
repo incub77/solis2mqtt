c050ad1d58cf3a496370aba16341392ffee61e42
incub <incub@gmx.net>
2021-10-24 10:48:57 +0200

    inverter description

--- mqtt_discovery.py
+++ mqtt_discovery.py
@@ -11,21 +11,25 @@ class DiscoverMsg():
                      "device_class": "",
                      "state_class": "",
                      "unit_of_measurement": "",
-                     "device": {"name": "solis2mqtt",
-                                "model": "solis2mqtt",
-                                "manufacturer": "incub",
+                     "device": {"name": "",
+                                "model": "",
+                                "manufacturer": "",
                                 "identifiers": "solis2mqtt"
                                 }
                      }
 
-    def __init__(self, name, id, unit, device_class, state_class="measurement"):
+    def __init__(self, name, id, unit, device_class, state_class,
+                 device_name, device_model, device_manufacturer):
         self.discover_msg = deepcopy(DiscoverMsg.DISCOVERY_MSG)
         self.discover_msg["name"] = name
-        self.discover_msg["state_topic"] = DiscoverMsg.DISCOVERY_MSG["state_topic"] + id
-        self.discover_msg["unique_id"] = "solis2mqtt/"+id
+        self.discover_msg["state_topic"] = device_name + "/" + id
+        self.discover_msg["unique_id"] =  device_name + "/" +id
         self.discover_msg["device_class"] = device_class
         self.discover_msg["state_class"] = state_class
         self.discover_msg["unit_of_measurement"] = unit
+        self.discover_msg["device"]["name"] = device_name
+        self.discover_msg["device"]["model"] = device_model
+        self.discover_msg["device"]["manufacturer"] = device_manufacturer
 
     def __str__(self):
         return json.dumps(self.discover_msg)
