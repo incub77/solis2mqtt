fd7e4a754fc56909fc729ed5caab2eacd649ef9f
incub <incub@gmx.net>
2021-10-24 22:33:10 +0200

    check if dict has key in __getitem__

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class DefaultDict(dict):
         return super().get(key) if super().get(key) is not None else self.defaults_dict.get(key)
 
     def __getitem__(self, key):
-        return super().__getitem__(key) if super().__getitem__(key) is not None else self.defaults_dict[key]
+        return super().__getitem__(key) if key in self \
+                                           and super().__getitem__(key) is not None else self.defaults_dict[key]
 
 
 class Config(DefaultDict):
@@ -22,9 +23,3 @@ class Config(DefaultDict):
         with open(cfg_file) as yaml_cfg_file:
             with open(defaults_cfg_file) as yaml_defaults_cfg_file:
                 super().__init__(yaml.load(yaml_cfg_file, yaml.Loader), yaml.load(yaml_defaults_cfg_file, yaml.Loader))
-
-
-
-
-
-
