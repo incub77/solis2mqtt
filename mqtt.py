855c94b89c1b2fecf689983e75a2abaeac3f0b10
incub <incub@gmx.net>
2021-10-30 12:20:10 +0200

    added ability to subscribe to topics enabled logging

--- mqtt.py
+++ mqtt.py
@@ -3,14 +3,24 @@ import paho.mqtt.client as mqtt_client
 class Mqtt(mqtt_client.Client):
     def __init__(self, client_id, cfg):
         super().__init__(client_id=client_id, clean_session=True)
+        self.enable_logger()
         self.username_pw_set(cfg['user'], cfg['passwd'])
         if cfg['use_ssl']:
             self.tls_set()
         if cfg['use_ssl'] and not cfg['validate_cert']:
             self.tls_insecure_set(True)
+        self.on_connect = self._on_connect_callback
         self.connect(cfg['url'], cfg['port'])
         self.loop_start()
-        
+        self.subscriptions = []
+
     def __del__(self):
-        if self.is_connected():
-            self.disconnect()
+        self.disconnect()
+
+    def _on_connect_callback(self, client, userdata, flags, rc):
+        if len(self.subscriptions):
+            self.subscribe(self.subscriptions)
+
+    def persistent_subscribe(self, topic):
+        self.subscriptions.append((topic, 0))
+        self.subscribe(topic)
