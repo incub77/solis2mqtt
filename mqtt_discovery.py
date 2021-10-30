e515c781e9fc12363acfaf5ecbe7b1f556d1490f
incub <incub@gmx.net>
2021-10-30 12:21:21 +0200

    renamed DiscoveryMsg to DiscoveryMsgSensor added DiscoveryMsgNumber

--- mqtt_discovery.py
+++ mqtt_discovery.py
@@ -4,9 +4,9 @@ from copy import deepcopy
 # Generate MQTT discovery message for home-assistant
 # for more info: https://www.home-assistant.io/docs/mqtt/discovery/
 
-class DiscoverMsg():
+class DiscoverMsgSensor():
     DISCOVERY_MSG = {"name": "",
-                     "state_topic": "solis2mqtt/",
+                     "state_topic": "",
                      "unique_id": "",
                      "device_class": "",
                      "state_class": "",
@@ -14,13 +14,14 @@ class DiscoverMsg():
                      "device": {"name": "",
                                 "model": "",
                                 "manufacturer": "",
-                                "identifiers": "solis2mqtt"
+                                "identifiers": "solis2mqtt",
+                                "sw_version": "solis2mqtt "
                                 }
                      }
 
     def __init__(self, name, id, unit, device_class, state_class,
-                 device_name, device_model, device_manufacturer):
-        self.discover_msg = deepcopy(DiscoverMsg.DISCOVERY_MSG)
+                 device_name, device_model, device_manufacturer, version):
+        self.discover_msg = deepcopy(DiscoverMsgSensor.DISCOVERY_MSG)
         self.discover_msg["name"] = name
         self.discover_msg["state_topic"] = device_name + "/" + id
         self.discover_msg["unique_id"] =  device_name + "/" +id
@@ -30,6 +31,40 @@ class DiscoverMsg():
         self.discover_msg["device"]["name"] = device_name
         self.discover_msg["device"]["model"] = device_model
         self.discover_msg["device"]["manufacturer"] = device_manufacturer
+        self.discover_msg["device"]["sw_version"] += str(version)
+
+    def __str__(self):
+        return json.dumps(self.discover_msg)
+
+class DiscoverMsgNumber():
+    DISCOVERY_MSG = {"name": "",
+                     "state_topic": "",
+                     "command_topic": "",
+                     "unique_id": "",
+                     "min": "",
+                     "max": "",
+                     "step": "",
+                     "device": {"name": "",
+                                "model": "",
+                                "manufacturer": "",
+                                "identifiers": "solis2mqtt",
+                                "sw_version": "solis2mqtt"
+                                }
+                     }
+
+    def __init__(self, name, id, min, max, step, device_name, device_model, device_manufacturer, version):
+        self.discover_msg = deepcopy(DiscoverMsgNumber.DISCOVERY_MSG)
+        self.discover_msg["name"] = name
+        self.discover_msg["state_topic"] = device_name + "/" + id
+        self.discover_msg["command_topic"] = device_name + "/" + id + "/set"
+        self.discover_msg["unique_id"] =  device_name + "/" +id
+        self.discover_msg["min"] = min
+        self.discover_msg["max"] = max
+        self.discover_msg["step"] = step
+        self.discover_msg["device"]["name"] = device_name
+        self.discover_msg["device"]["model"] = device_model
+        self.discover_msg["device"]["manufacturer"] = device_manufacturer
+        self.discover_msg["device"]["sw_version"] += str(version)
 
     def __str__(self):
         return json.dumps(self.discover_msg)
