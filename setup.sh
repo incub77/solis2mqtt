72a1f0e83b974da34adbd56574790ae113fdad7d
incub <incub@gmx.net>
2021-11-14 23:38:07 +0100

    absolute paths

--- setup.sh
+++ setup.sh
@@ -7,22 +7,23 @@ echo "### Installing requirements ###"
 pip3 install -r requirements.txt
 
 echo "### Creating config file ###"
-cp config.yaml.minimal config.yaml
+cp /opt/solis2mqtt/config.yaml.minimal /opt/solis2mqtt/config.yaml
 
 echo "### Creating service user ###"
 addgroup --system solis2mqtt
 adduser --system --no-create-home --ingroup solis2mqtt solis2mqtt
+# to get access to /dev/ttyUSB*
 usermod -a -G dialout solis2mqtt
 
 echo "### Changing ownership ###"
-chown -R solis2mqtt:solis2mqtt .
+chown -R solis2mqtt:solis2mqtt /opt/solis2mqtt
 
-echo "### Creating runtime directory ###"
+echo "### Creating run directory ###"
 mkdir /run/solis2mqtt
 chown -R solis2mqtt:solis2mqtt /run/solis2mqtt
 
 echo "### Setting up systemd ###"
-cp ./provisioning/etc/systemd/system/solis2mqtt.service /etc/systemd/system/
+cp /opt/solis2mqtt/provisioning/etc/systemd/system/solis2mqtt.service /etc/systemd/system/
 systemctl enable solis2mqtt
 
 
